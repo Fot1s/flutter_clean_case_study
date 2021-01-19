fbbb74aabf40b13ef1ec94fd0a39e9e6f8079e20
Fotis Chatzinikos <fotis.chatzinikos@gmail.com>
2021-01-19 23:35:57 +0200

    added functionality to show back or burger icon, added a top right fab to show the category (can also pop to categories)

--- lib/app/widgets/scaffold_with_drawer.dart
+++ lib/app/widgets/scaffold_with_drawer.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_case_study/domain/entities/category.dart';
 
 import 'drawer/app_drawer.dart';
 
-class ScaffoldWithDrawer extends StatelessWidget
-{
-  ScaffoldWithDrawer({Key key, this.title, this.body}) : super(key: key);
+class ScaffoldWithDrawer extends StatelessWidget {
+  ScaffoldWithDrawer(
+      {Key key, this.title, this.body, this.showBackInsteadOfDrawer = false, this.withCategoryHero})
+      : super(key: key);
 
   final String title;
-  final Widget body ;
+  final Widget body;
+
+  final bool showBackInsteadOfDrawer;
+
+  final Category withCategoryHero;
 
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: body,
-      drawer: AppDrawer(),
+        appBar: AppBar(
+          title: Text(title),
+          leading: showBackInsteadOfDrawer ? IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context),
+          ) : null,
+        ),
+        body: body,
+        drawer: AppDrawer(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+        floatingActionButton: withCategoryHero == null? null : FloatingActionButton(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.white,
+          heroTag: null,
+          child: Hero(
+            tag: withCategoryHero.uid,
+            child: Image.asset(
+              withCategoryHero.image,
+              fit: BoxFit.fill,
+            ),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        )
     );
   }
 }
